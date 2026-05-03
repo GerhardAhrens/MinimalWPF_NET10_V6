a60fafe6b018ef8dc2248f9d24509bc00f6ba6a7
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2026-05-03 12:31:08 +0200

    Add SingletonBase

---
+++ MinimalWPF_NET10_V6/Beispiele/Singleton/ConfigurationManager.cs
@@ -0,0 +1,32 @@
+﻿namespace MinimalWPF.Beispiele
+{
+    using System;
+    using System.Windows;
+
+    public class ConfigurationManager : SingletonBase<ConfigurationManager>, ISingletonInitializable
+    {
+        protected ConfigurationManager()
+        {
+        }
+
+        public string ApplicationName { get; private set; } = string.Empty;
+        public DateTime StartupTime { get; private set; }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Initialisierung läuft...");
+
+            // Beispielwerte laden
+            ApplicationName = "Meine NET 10 Anwendung";
+            StartupTime = DateTime.Now;
+
+            Console.WriteLine("Initialisierung abgeschlossen");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"App: {ApplicationName}");
+            Console.WriteLine($"Startup: {StartupTime}");
+        }
+    }
+}
---
+++ MinimalWPF_NET10_V6/TemplateCore/Base/SingletonBase.cs
@@ -0,0 +1,42 @@
+﻿namespace System.Windows
+{
+    using System;
+
+    public abstract class SingletonBase<T>
+            where T : class
+    {
+        private static readonly Lazy<T> _instance =
+            new Lazy<T>(() =>
+            {
+                var type = typeof(T);
+
+                // Erstellt Instanz über privaten/protected Konstruktor
+                var obj = (T)Activator.CreateInstance(type, nonPublic: true)!;
+
+                // Falls Initialisierung unterstützt wird
+                if (obj is ISingletonInitializable init)
+                {
+                    try
+                    {
+                        init.Initialize();
+                    }
+                    catch (Exception ex)
+                    {
+                        string errorText = $"Fehler bei der Initialisierung der Singleton-Instanz vom Typ {type.FullName}: {ex.Message}";
+                        throw;
+                    }
+                }
+
+                return obj;
+            },System.Threading.LazyThreadSafetyMode.ExecutionAndPublication);
+
+#pragma warning disable CA1000 // Statische Member nicht in generischen Typen deklarieren
+        public static T Instance => _instance.Value;
+#pragma warning restore CA1000 // Statische Member nicht in generischen Typen deklarieren
+    }
+
+    public interface ISingletonInitializable
+    {
+        void Initialize();
+    }
+}
